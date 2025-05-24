31cf8eb0d6abdef0d1db55b067a69f5f72f3f61b
Mansura <badalovamansura40@gmail.com>
2025-05-24 15:58:04 +0400

    added succesfully

--- src/main/java/controller/FamilyController.java
+++ src/main/java/controller/FamilyController.java
@@ -4,6 +4,7 @@ import model.*;
 import service.FamilyService;
 
 import java.util.List;
+import java.util.Set;
 
 public class FamilyController {
 
@@ -13,70 +14,78 @@ public class FamilyController {
         this.familyService = familyService;
     }
 
-
-    public List< Family> getAllFamilies(){
-          return familyService.getAllFamilies();
+    public List<Family> getAllFamilies() {
+        return familyService.getAllFamilies();
     }
-    public void displayAllFamilies(){
+
+    public void displayAllFamilies() {
         familyService.displayAllFamilies();
     }
-    public List< Family> getFamiliesBiggerThan(int number){
-        if(number<0){
+
+    public List<Family> getFamiliesBiggerThan(int number) {
+        if (number < 0) {
             return null;
         }
         return familyService.getFamiliesBiggerThan(number);
     }
-    public List< Family> getFamiliesLessThan(int number){
-        if(number<0){
+
+    public List<Family> getFamiliesLessThan(int number) {
+        if (number < 0) {
             return null;
         }
         return familyService.getFamiliesLessThan(number);
     }
-    public int countFamiliesWithMemberNumber(int memberNumber){
-        if(memberNumber<0){
+    public int countFamiliesWithMemberNumber(int memberNumber) {
+        if (memberNumber < 0) {
             return 0;
         }
         return familyService.countFamiliesWithMemberNumber(memberNumber);
+    }
+    public Family createNewFamily(Human mother, Human father) {
+        if (mother == null || father == null) {
+            return null;
+        }
+        return familyService.createNewFamily(mother, father);
 
-     }
-     public Family createNewFamily(Human mother, Human father){
-          if(mother == null || father == null){
-              return null;
-          }
-           return null;
-          // TODO will change
+    }
 
-     }
-     public Boolean deleteFamilyByIndex(int index){
+    public Boolean deleteFamilyByIndex(int index) {
 
-        if(index<0){
+        if (index < 0) {
             return false;
         }
         return familyService.deleteFamilyByIndex(index);
-     }
-     public  Family bornChild(Family family,String masculine,String feminine){
-         return  familyService.bornChild(family,masculine,feminine);
-
-     }
-     public Family adoptChild(Family family, Human child){
-          return null;
-     }
-     public Boolean deleteAllChildrenOlderThen(int age){
-          return false;
-
-     }
-     public int count(int number){
-          return 0;
-     }
-     public  Family getFamilyById(int index){
-          return null;
-     }
-    public Pet getPets(int index){
-          return null;
+    }
+    public Family bornChild(Family family, String masculine, String feminine) {
+        return familyService.bornChild(family, masculine, feminine);
 
     }
-    public Boolean addPet(Pet pet,int index){
-          return false;
+    public Family adoptChild(Family family, Human child) {
+
+        return familyService.adoptChild(family, child);
+    }
+
+    public Boolean deleteAllChildrenOlderThen(int age) {
+        return familyService.deleteAllChildrenOlderThen(age);
+
+    }
+
+    public int count(int number) {
+
+        return familyService.count();
+    }
+
+    public Family getFamilyById(int index) {
+        return familyService.getFamilyById(index);
+    }
+
+    public Set<Pet> getPets(int index) {
+        return familyService.getPets(index);
+    }
+
+    public Boolean addPet(int index, Pet pet) {
+
+        return familyService.addPet(index, pet);
     }
 
 }
