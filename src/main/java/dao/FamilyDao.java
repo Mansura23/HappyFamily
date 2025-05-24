8580befcd1e1781731367ae5723f7b4e7e48646b
Zulfiyye <zulfiyyerehimova347@gmail.com>
2025-05-24 14:51:23 +0400

    dao

---
+++ src/main/java/dao/FamilyDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+
+import model.Family;
+
+import java.util.List;
+
+public interface FamilyDao {
+    List<Family> getAllFamilies();
+    Family getFamilyByIndex(int index);
+    boolean deleteFamily(int index);
+    boolean deleteFamily(Family family);
+    void saveFamily(Family family);
+}
---
+++ src/main/java/dao/impl/CollectionFamilyDaoImpl.java
@@ -0,0 +1,51 @@
+package dao.impl;
+
+import dao.FamilyDao;
+import model.Family;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionFamilyDaoImpl implements FamilyDao {
+    private  List<Family> families = new ArrayList<>();
+
+    public List<Family> getAllFamilies() {
+        return new ArrayList<>(families);
+    }
+
+    @Override
+    public Family getFamilyByIndex(int index) {
+        if(index>=0 && index<families.size()){
+            return  families.get(index);
+        }
+        System.out.println("Invalid index");
+        return null;
+
+    }
+
+    @Override
+    public boolean deleteFamily(int index) {
+        if(index>=0 && index<families.size()){
+             families.remove(index);
+             return  true;
+        }
+        System.out.println("Invalid index");
+        return false;
+    }
+
+    @Override
+    public boolean deleteFamily(Family family) {
+        return  families.remove(family);
+    }
+
+    @Override
+    public void saveFamily(Family family) {
+        if(families.contains(family)){
+             int index= families.indexOf(family);
+             families.set(index,family);
+        }
+        else{
+            families.add(family);
+        }
+    }
+}
