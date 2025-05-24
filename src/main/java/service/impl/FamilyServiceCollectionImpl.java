634d37a3058e523537d15785cf19e64db2f5f344
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-05-24 15:03:11 +0400

    Constructor Dependecy Added!

--- src/main/java/service/impl/FamilyServiceCollectionImpl.java
+++ src/main/java/service/impl/FamilyServiceCollectionImpl.java
@@ -1,5 +1,6 @@
 package service.impl;
 
+import dao.FamilyDao;
 import model.Family;
 import service.FamilyServiceCollection;
 
@@ -7,12 +8,33 @@ import java.util.List;
 
 public class FamilyServiceCollectionImpl implements FamilyServiceCollection {
 
+    private final FamilyDao familyDao;
+
+    public FamilyServiceCollectionImpl(FamilyDao familyDao) {
+        this.familyDao = familyDao;
+    }
+
     public List<Family> getAllFamilies() {
         return null;
     }
 
     public List<Family> displayAllFamilies() {
         return null;
+    }
+
+    @Override
+    public List<Family> getFamiliesBiggerThan() {
+        return List.of();
+    }
+
+    @Override
+    public List<Family> getFamiliesLessThan() {
+        return List.of();
+    }
+
+    @Override
+    public List<Family> countFamiliesWithMemberNumber() {
+        return List.of();
     }
 
 }
